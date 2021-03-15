b04f33ec8caea59ba2e9be87d7b230955672e90d
jerin1406 <mca519@rajagiri.edu>
2021-03-14 22:39:20 -0700

    website

---
+++ index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-success">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="index.php">RCSS</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="galary.php">Gallary</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="aboutdepartment.php">Department</a>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      QuickLinks
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a class="dropdown-item" href="studentlogin.php">Student Login</a></li>
+                      <li><a class="dropdown-item" href="facultylogin.php">Faculty login</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="galary.php">Photo Galary</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="aboutus.php" tabindex="-1" aria-disabled="true">About Us</a>
+                  </li>
+                </ul>
+                <ul class="list-unstyled" style="font-size: 20px;color: rgb(243, 247, 243);">
+                    <li>Phone: 91 - 484 - 2660601</li>
+                    <li>Fax: 91 - 484 - 2426578+</li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+
+          <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+              <span class="navbar-text">
+                <div class="container">
+                    <div class="row">
+                        <div class="col col-12 col-sm-2 col-lg-2 col-md-2 col-xl-2 col-xxl-2">
+                            <img height="10px" src="https://media-exp1.licdn.com/dms/image/C560BAQEuhhXCRxgRxw/company-logo_200_200/0/1590932047715?e=2159024400&v=beta&t=YusLl2dEDL__7wnUXLQpE1qYvFsK-T71O1ZYL5Ybf-E" class="img-thumbnail" alt="...">
+                        </div>
+                        <div class="col col-12 col-sm-10 col-lg-10 col-md-10 col-xl-10 col-xxl-10">
+                            <strong st>Vision:</strong>To become a centre par excellence of learning, unique in experience, value based in its approach, and pioneering in its efforts for enriching and fulfilling LIFE.
+            <br><strong>Mission:</strong>To facilitate comprehensive and integrated development of individuals imbued with righteousness and courage of conviction, to effectively function as social beings
+                        </div>
+                    </div>
+                </div>
+              </span>
+            </div>
+          </nav>
+
+        </nav>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#" style="font-family: Georgia, 'Times New Roman', Times, serif;">NEW UPDATES  : <MARQUEE style="font-family: Georgia, 'Times New Roman', Times, serif; ">
+              UG - 2021 Admissions - Applications Open (BBA, BCom, BSc, BSW) International Women’s Day Celebrations  </MARQUEE>  </a>
+           
+            </div>
+          </div>
+        </nav>      
+
+    <div class="container">
+        
+        <div class="row" >
+            <div class="col col-12 col-sm-12 col-md-12"></div>
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img height="700px" src="https://rajagiri.edu/uploads/home-banner/5/5.jpg" class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img height="700px" src="images\itemImage_15519456887633.jpg " class="d-block w-100" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img height="700px" src="images\14.jpg" class="d-block w-100" alt="...">
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"  data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"  data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+        </div>
+        <div class="row">
+            <div class="col col-12 col-sm-6 col-lg-6 col-md-6 col-xl-6 col-xxl-6">
+                <h2><center><u>Upcoming Events</u></center></h2>
+                <div class="accordion" id="accordionExample">
+                    <div class="accordion-item">
+                      <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                          <b>  Academics</b>
+                        </button>
+                      </h2>
+                      <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                        <br>Dept. of Social Work<br>
+Dept. of Computer Science<br>
+Dept. of Library and Information Science<br>
+Dept. of Business Administration<br>
+Dept. of Personnel Management<br>
+Dept. of Commerce<br>
+Dept. of Psychology<br>
+Dept. of Languages<br>
+Dept. of Biosciences<br>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="accordion-item">
+                      <h2 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                           <b> USEFUL LINKS</b>
+                        </button>
+                      </h2>
+                      <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div class="accordion-body">
+                        <br>Centre for Social Work<br>
+Centre for Management Studies<br>
+Rajagiri Research Institute<br>
+ICRS<br>
+Funded Projects<br>
+Faculty Publications<br>
+e-Contents<br>
+e-Payment Policy<br>
+Careers<br>
+                        </div>
+                      </div>
+                    </div>
+                    
+                  </div>
+            </div>
+            <div class="col col-12 col-sm-6 col-lg-6 col-md-6 col-xl-6 col-xxl-6">
+                <h3>
+                   <center>Welcome To Rajagiri</center> 
+                </h3>
+                
+                <center><img height="30px" src="images\tuxpi.com.1550751743.jpg" class="img-fluid" alt="..."></center>
+                <p></p>
+                Rajagiri College of Social Sciences (RCSS), together with its sister educational concerns, owes its existence to the CMI (Carmelites of Mary Immaculate) fathers, the first ever indigenous religious congregation for men in the Syrian Catholic tradition of Christianity in India. The CMIs drawing inspiration from their founding father St. Kuriakose Elias Chavara, a great visionary, reformer and religious leader of the 19th century, have proven themselves worthy of that heritage in the field of education by establishing institutions of excellence imparting quality education, across the length and breadth of the State, and in various parts of India. At present, the CMIs manage a network of more than 800 Schools, 13 Special Schools, 41 Arts, Science, Commerce & Physical Education Colleges, 10 B.Ed. Colleges, 4 Nursing Colleges, 4 Engineering Colleges, one Medical College, one University, 3 Law Colleges, 6 Research Centres, 7 Technical Institutes, 41 Hostels & Boarding Houses, 17 Cultural Centres, 2 Super Speciality Hospitals and 114 Non-formal Education Centres.  
+                </p>
+            </div>
+        </div>
+    </div>
+
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>    
+</body>
+</html>
